2d190263475de5f1a538c92ce8ee930d14a6aba3
DAT <tdat.nguyen93@gmail.com>
2017-03-13 14:15:34 -0700

    Updated Projects

---
+++ utils/heatmap.py
@@ -0,0 +1,15 @@
+def add_heat(heatmap, bbox_list):
+    # Iterate through list of bboxes
+    for box in bbox_list:
+        # Add += 1 for all pixels inside each bbox
+        # Assuming each "box" takes the form ((x1, y1), (x2, y2))
+        heatmap[box[0][1]:box[1][1], box[0][0]:box[1][0]] += 1
+
+    # Return updated heatmap
+    return heatmap
+
+def apply_threshold(heatmap, threshold):
+    # Zero out pixels below the threshold
+    heatmap[heatmap <= threshold] = 0
+    # Return thresholded map
+    return heatmap
